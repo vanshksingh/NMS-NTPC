95d12e46f71b5c6114326d58445a41cbad697dbf
Vansh Kumar Singh <114809624+vanshksingh@users.noreply.github.com>
2023-12-11 16:23:05 +0530

    Update basic nms volatile.py
    known issue : colour not implemented

--- basic nms volatile.py
+++ basic nms volatile.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import subprocess
 import platform
 
@@ -13,8 +14,8 @@ class DeviceMonitorApp(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Device Monitor")
-        self.geometry("800x600")
-        self.devices = {}  # Store devices as {name: [IP, status_label]}
+        self.geometry("1000x700")
+        self.devices = {}  # Store devices as {name: [IP, treeview_item]}
         self.setup_ui()
 
     def setup_ui(self):
@@ -30,24 +31,33 @@ class DeviceMonitorApp(tk.Tk):
         add_button = tk.Button(self.entry_frame, text="Add Device", command=self.add_device)
         add_button.grid(row=0, column=2, padx=5)
 
-        self.device_frame = tk.Frame(self)
-        self.device_frame.pack()
+        # Treeview setup
+        self.tree = ttk.Treeview(self, columns=("Name", "IP", "Status"), show='headings')
+        self.tree.heading("Name", text="Name")
+        self.tree.heading("IP", text="IP")
+        self.tree.heading("Status", text="Status")
+        self.tree.column("Name", width=200)
+        self.tree.column("IP", width=200)
+        self.tree.column("Status", width=200)
+        self.tree.pack(pady=20)
 
     def add_device(self):
         name = self.name_entry.get()
         ip = self.ip_entry.get()
         if name and ip:
-            label = tk.Label(self.device_frame, text=f"{name} ({ip}): Checking...")
-            label.pack()
-            self.devices[name] = [ip, label]
+            item = self.tree.insert("", tk.END, values=(name, ip, "Checking..."))
+            self.devices[name] = [ip, item]
             self.name_entry.delete(0, tk.END)
             self.ip_entry.delete(0, tk.END)
 
+    def update_device_status(self, name, status):
+        item = self.devices[name][1]
+        self.tree.item(item, values=(name, self.devices[name][0], "Online" if status else "Offline"))
+
     def monitor_devices(self):
-        for name, [ip, label] in self.devices.items():
+        for name, [ip, _] in self.devices.items():
             status = ping(ip)
-            label.config(text=f"{name} ({ip}): {'Online' if status else 'Offline'}",
-                         fg='green' if status else 'red')
+            self.update_device_status(name, status)
         self.after(5000, self.monitor_devices)  # Schedule next check in 5 seconds
 
 if __name__ == "__main__":
